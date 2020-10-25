2e0bd59221f4128fa91b358e23c0b531602eedc9
Wayne <bwh.buaa@gmail.com>
2020-10-25 09:59:34 -0400

    first version

--- main.py
+++ main.py
@@ -133,15 +133,15 @@ if __name__ == '__main__':
         output = MLP_plain(h_size=args.hidden_size_rnn, embedding_size=args.embedding_size_output,
                            y_size=args.max_prev_node).cuda()
     elif 'GraphRNN_RNN' in args.note:
-        rnn = GRU_plain(input_size=args.node_feature_input_dim, embedding_size=args.embedding_size_rnn,
+        rnn = GRU_plain(input_size=args.max_node_feature_num+args.max_child_node, embedding_size=args.embedding_size_rnn,
                         hidden_size=args.hidden_size_rnn, num_layers=args.num_layers, has_input=True,
                         has_output=True, output_size=args.hidden_size_rnn_output).cuda()
         node_f_gen = MLP_plain(h_size=args.hidden_size_rnn_output, embedding_size=args.embedding_size_output,
-                               y_size=args.max_node_feature_num).cuda()
-        output = GRU_plain(input_size=args.edge_feature_output_dim, embedding_size=args.embedding_size_rnn_output,
+                               y_size=args.max_node_feature_num+args.max_child_node).cuda()
+        output = GRU_plain(input_size=args.max_node_feature_num+args.max_child_node, embedding_size=args.embedding_size_rnn_output,
                            hidden_size=args.hidden_size_rnn_output, num_layers=args.num_layers, has_input=True,
                            has_output=True,
-                           output_size=args.edge_feature_output_dim).cuda()  # TODO: understand input_size, output_size ?
+                           output_size=args.max_node_feature_num+args.max_child_node).cuda()  # TODO: understand input_size, output_size ?
         edge_f_gen = None
         # edge_f_gen = MLP_plain(h_size=args.edge_feature_output_dim, embedding_size=args.embedding_size_output, y_size=args.edge_feature_output_dim).cuda()
 
